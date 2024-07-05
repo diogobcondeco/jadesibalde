618db536018bec35ac754c121d42ae7d4298a233
Diogo Condêço <diogobcondeco@gmail.com>
2024-07-05 18:43:48 +0100

    fix format

--- components/ServicesModal.tsx
+++ components/ServicesModal.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import { servicesList } from "@/lib/data";
@@ -27,57 +27,62 @@ const ServicesModal = () => {
   return (
     <>
       <div className="flex justify-center md:justify-end mt-4">
-          <button
-            onClick={openModal}
-            className="bg-white text-themeColor mt-2 transition-transform duration-300 transform hover:scale-110 underline"
-          >
-            Ver Lista Completa
-          </button>
-        </div>
-        {isModalOpen && (
-          <div
-            className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
-            onClick={handleBackdropClick} // Close modal on backdrop click
-          >
-            <div className="bg-white rounded-lg p-8 max-w-full md:max-w-4xl w-full max-h-[calc(100vh-10rem)] overflow-y-auto relative mx-4 md:mx-8 lg:mx-0">
-              <button
-                onClick={closeModal}
-                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none z-50"
+        <button
+          onClick={openModal}
+          className="bg-white text-themeColor mt-2 transition-transform duration-300 transform hover:scale-110 underline"
+        >
+          Ver Lista Completa
+        </button>
+      </div>
+      {isModalOpen && (
+        <div
+          className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
+          onClick={handleBackdropClick} // Close modal on backdrop click
+        >
+          <div className="bg-white rounded-lg p-8 max-w-full md:max-w-4xl w-full max-h-[calc(100vh-10rem)] overflow-y-auto relative mx-4 md:mx-8 lg:mx-0">
+            <button
+              onClick={closeModal}
+              className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none z-50"
+            >
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-              <h2 className="text-2xl font-bold mb-4">Todos os Serviços</h2>
-              <div className="grid grid-cols-1 gap-6">
-                {servicesList.map((serviceCategory, index) => (
-                  <div key={index} className="mb-4">
-                    <h3 className="text-lg font-bold">{serviceCategory.title}</h3>
-                    <ul className="list-disc list-inside">
-                      {serviceCategory.list.map((service, idx) => (
-                        <li key={idx}>{service}</li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
-              </div>
-              <button
-                onClick={closeModal}
-                className="mt-4 bg-themeColor text-white font-bold py-2 px-4 rounded hover:bg-opacity-80 w-full md:w-fit"
-              >
-                Fechar
-              </button>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <h2 className="text-2xl font-bold mb-4">Todos os Serviços</h2>
+            <div className="grid grid-cols-1 gap-6">
+              {servicesList.map((serviceCategory, index) => (
+                <div key={index} className="mb-4">
+                  <h3 className="text-lg font-bold">{serviceCategory.title}</h3>
+                  <ul className="list-disc list-inside">
+                    {serviceCategory.list.map((service, idx) => (
+                      <li key={idx}>{service}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
             </div>
+            <button
+              onClick={closeModal}
+              className="mt-4 bg-themeColor text-white font-bold py-2 px-4 rounded hover:bg-opacity-80 w-full md:w-fit"
+            >
+              Fechar
+            </button>
           </div>
-        )}
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default ServicesModal;
